2075ccbe4109aba7d3a22dd57a9e5a81df0c55a1
Johanx22x <johanrodsa2210@gmail.com>
2022-11-23 16:45:42 -0600

    New version with special characters to help the parser

--- Arch-IoT-Project.ino
+++ Arch-IoT-Project.ino
@@ -4,7 +4,7 @@
  * This is the Arduino source code for the device
  *
  * @author Johan Rodriguez, Gabriel Alfaro, Christian Ferreto
- * @version 1.0
+ * @version 1.1
  */
 
 #include "DHT.h" // Library for DHT11 sensor
